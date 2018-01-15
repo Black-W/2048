79075cc4d8a5047b1c4475bd5bf18d2430b25b87
Black-W <34498625+Black-W@users.noreply.github.com>
2018-01-15 13:50:46 +0800

    Add files via upload

--- source.c
+++ source.c
@@ -1,46 +1,47 @@
-/*Author:Zhenwei Wang
+/*2048.c
+Author:Zhenwei Wang
 Email:664129552@qq.com
 Data:2017/12/18
 */
 #include <stdio.h>
-#include <conio.h>		//锟斤拷锟斤拷 getch()锟斤拷锟斤拷锟铰凤拷锟斤拷锟斤拷锟斤拷锟斤拷锟叫讹拷 
-#include <stdlib.h> 	//锟斤拷锟斤拷srand(),rand(),system(),exit(),锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷,实锟街★拷锟斤拷锟斤拷锟斤拷,锟皆硷拷锟斤拷止锟斤拷锟斤拷 
-#include <time.h>		//锟斤拷锟斤拷time(),锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷
-#define SIZE 4			//锟斤拷锟教边筹拷
-#define WINSCORE 2048	//锟叫讹拷胜锟斤拷锟侥凤拷锟斤拷 
-void start_game();		//锟斤拷始锟斤拷戏
-void initial_game();	//锟斤拷始锟斤拷锟斤拷戏锟斤拷锟斤拷
-//锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟狡讹拷
-void rotate_board();	//顺时锟斤拷锟斤拷转锟斤拷锟斤拷90锟斤拷
+#include <conio.h>		//包含 getch()，按下方向键立刻判断 
+#include <stdlib.h> 	//包含srand(),rand(),system(),exit(),用来生成随机数,实现“清屏”,以及终止程序 
+#include <time.h>		//包含time(),用来获得随机数种子
+#define SIZE 4			//棋盘边长
+#define WINSCORE 2048	//判断胜利的分数 
+void start_game();		//开始游戏
+void initial_game();	//初始化游戏棋盘
+//向上下左右移动
+void rotate_board();	//顺时针旋转棋盘90度
 void move_up();
 void move_left();
 void move_right();
 void move_down();
 
-void add_random();	//锟斤拷board锟斤拷锟斤拷锟斤拷锟斤拷一锟斤拷锟斤拷锟斤拷锟斤拷2锟斤拷锟斤拷4锟斤拷2锟侥革拷锟斤拷锟斤拷4锟斤拷锟斤拷锟斤拷锟斤拷
-int count_empty();	//锟斤拷锟截碉拷前board锟斤拷锟斤拷锟斤拷为锟秸的革拷锟斤拷锟斤拷0锟斤拷示锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷
-void check_game_ended();	//锟斤拷锟斤拷锟斤拷戏锟角凤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷game_ended=1锟斤拷未锟斤拷锟斤拷game_ended=0
-void check_game_win();	//锟斤拷锟斤拷锟斤拷戏锟角凤拷胜锟斤拷锟斤拷胜锟斤拷game_win=1,未胜锟斤拷game_win=0
-void print_board();		//锟斤拷锟斤拷锟斤拷戏锟斤拷锟斤拷
+void add_random();	//向board数组添加一个随机数2或者4（2的概率是4的两倍）
+int count_empty();	//返回当前board数组中为空的个数（0表示棋盘已满）
+void check_game_ended();	//检查游戏是否结束，结束game_ended=1，未结束game_ended=0
+void check_game_win();	//检查游戏是否胜利，胜利game_win=1,未胜利game_win=0
+void print_board();		//输出游戏棋盘
 
 
-int board[SIZE][SIZE];	//锟斤拷锟斤拷锟斤拷戏锟斤拷锟斤拷锟斤拷锟斤拷
-int need_add_random=0;	//锟角凤拷锟斤拷要锟斤拷锟斤拷元锟截憋拷锟角ｏ拷1锟斤拷示锟斤拷要锟斤拷0锟斤拷示锟斤拷锟斤拷要锟斤拷
-int game_ended=0;		//锟斤拷戏锟斤拷锟斤拷锟斤拷锟角ｏ拷1锟斤拷示锟斤拷锟斤拷锟斤拷0锟斤拷示未锟斤拷锟斤拷锟斤拷
-int game_win=0;		//锟斤拷戏胜锟斤拷锟斤拷锟角ｏ拷1锟斤拷示胜锟斤拷锟斤拷0锟斤拷示未胜锟斤拷锟斤拷
-int score=0;			//锟斤拷戏锟矫凤拷
+int board[SIZE][SIZE];	//定义游戏棋盘数组
+int need_add_random=0;	//是否需要添加元素标记（1表示需要，0表示不需要）
+int game_ended=0;		//游戏结束标记（1表示结束，0表示未结束）
+int game_win=0;		//游戏胜利标记（1表示胜利，0表示未胜利）
+int score=0;			//游戏得分
 int main() {
-	while(1) {						//循锟斤拷一锟轿撅拷锟斤拷一锟斤拷锟铰碉拷锟斤拷戏
-		initial_game();				//锟斤拷始锟斤拷锟斤拷锟斤拷
-		start_game();				//锟斤拷戏锟斤拷锟叫猴拷锟斤拷
-		if(game_ended) {			//锟叫革拷
+	while(1) {						//循环一次就是一次新的游戏
+		initial_game();				//初始化棋盘
+		start_game();				//游戏运行函数
+		if(game_ended) {			//判负
 			printf("                       GAME OVER.   PLAY AGAIN? (Y/N)");
 			int ch;
 			while(ch=getch()) {
-				if(ch=='y'||ch=='Y')break;	//锟斤拷锟斤拷锟斤拷戏
-				if(ch=='n'||ch=='N')exit(0);//锟剿筹拷锟斤拷戏
+				if(ch=='y'||ch=='Y')break;	//继续游戏
+				if(ch=='n'||ch=='N')exit(0);//退出游戏
 			}
-		} else if(game_win) {		//锟斤拷胜
+		} else if(game_win) {		//判胜
 			printf("                        YOU WIN.   PLAY AGAIN? (Y/N)");
 			int ch;
 			while(ch=getch()) {
@@ -54,7 +55,7 @@ int main() {
 void start_game() {
 	int ch;
 	while(!game_ended&&!game_win) {
-		need_add_random=0;		//每锟斤拷锟狡讹拷前锟斤拷锟斤拷锟接憋拷锟斤拷锟斤拷锟斤拷
+		need_add_random=0;		//每次移动前将添加标记置零
 		ch=getch();
 		switch(ch) {
 			case'w':
@@ -85,87 +86,87 @@ void start_game() {
 			default:
 				;
 		}
-		if(need_add_random) {	//锟斤拷锟接憋拷锟斤拷为1时锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷
+		if(need_add_random) {	//添加标记为1时，向棋盘中添加随机数
 			add_random();
 			print_board();
 		}
-		check_game_ended();		//锟斤拷锟斤拷锟斤拷戏锟角凤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟睫革拷game_ended=1
-		check_game_win();		//锟斤拷锟斤拷锟斤拷戏锟角凤拷胜锟斤拷锟斤拷锟斤拷胜锟斤拷锟睫革拷game_win=1
+		check_game_ended();		//检查游戏是否结束，若结束修改game_ended=1
+		check_game_win();		//检查游戏是否胜利，若胜利修改game_win=1
 	}
 }
 void initial_game() {
 	game_win=0;
-	game_ended=0;							//锟斤拷始锟斤拷锟斤拷志
+	game_ended=0;							//初始化标志
 	score=0;
 	int x,y;
 	for(x=0; x<SIZE; x++) {
-		for(y=0; y<SIZE; y++)board[x][y]=0;	//锟斤拷锟斤拷锟斤拷锟斤拷
+		for(y=0; y<SIZE; y++)board[x][y]=0;	//棋盘清零
 	}
 	add_random();
-	add_random();						//锟斤拷锟教硷拷锟斤拷2锟斤拷锟斤拷锟斤拷锟斤拷
-	print_board();							//锟斤拷印锟斤拷锟斤拷
+	add_random();						//棋盘加入2个随机数
+	print_board();							//打印棋盘
 }
-void rotate_board() {	//顺时锟斤拷锟斤拷转锟斤拷锟斤拷90锟斤拷
+void rotate_board() {	//顺时针旋转棋盘90度
 	int board2[SIZE][SIZE];
 	int x,y;
-	for(x=0; x<SIZE; x++) {				//锟斤拷board2锟芥储Board锟斤拷转锟斤拷锟侥斤拷锟斤拷
+	for(x=0; x<SIZE; x++) {				//用board2存储Board旋转后的结果
 		for(y=0; y<SIZE; y++) {
 			board2[y][SIZE-1-x]=board[x][y];
 		}
 	}
-	for(x=0; x<SIZE; x++) {				//锟斤拷锟斤拷锟斤拷锟脚伙拷Board
+	for(x=0; x<SIZE; x++) {				//将结果放回Board
 		for(y=0; y<SIZE; y++) {
 			board[x][y]=board2[x][y];
 		}
 	}
 }
 void move_up() {
-	int x,y,p;								//y=锟斤拷锟斤拷锟斤拷x=锟斤拷锟斤拷锟斤拷p=扫锟斤拷锟斤拷锟斤拷
-	for(y=0; y<SIZE; y++)	    			//锟斤拷锟斤拷锟斤拷锟铰合诧拷锟斤拷同锟斤拷元锟斤拷
+	int x,y,p;								//y=列数，x=行数，p=扫描行数
+	for(y=0; y<SIZE; y++)	    			//从上向下合并相同的元素
 		for(x=0; x<SIZE; x++) {
 			if(board[x][y]==0);
-			else {							//锟揭碉拷每一锟叫碉拷一锟斤拷锟斤拷锟斤拷锟斤拷元锟斤拷board[x][y]
+			else {							//找到每一列第一个非零的元素board[x][y]
 				for(p=x+1; p<SIZE; p++) {
 					if(board[p][y]==0);
-					else if(board[p][y]==board[x][y]) {	//锟揭碉拷锟斤拷锟斤拷锟叫碉拷一锟斤拷锟斤拷锟斤拷元锟斤拷锟斤拷锟斤拷锟侥凤拷锟斤拷元锟斤拷board[p][y]
-						board[x][y]*=2;		//锟较诧拷锟斤拷锟斤拷一锟斤拷锟斤拷锟斤拷元锟斤拷
-						board[p][y]=0;		//锟斤拷一锟斤拷元锟斤拷锟斤拷为0
-						score+=board[x][y];//每锟较筹拷一锟斤拷元锟截ｏ拷锟斤拷锟铰得凤拷
-						x=p;				//锟斤拷p锟斤拷始锟斤拷锟斤拷锟斤拷锟斤拷寻锟斤拷锟铰的碉拷一锟斤拷锟斤拷锟斤拷元锟截ｏ拷锟截革拷锟斤拷锟斤拷锟斤拷锟斤拷
-						need_add_random=1;  //每锟斤拷锟斤拷效锟狡讹拷锟斤拷锟斤拷锟斤拷要锟斤拷锟斤拷锟斤拷元锟斤拷
+					else if(board[p][y]==board[x][y]) {	//找到离该列第一个非零元素最近的非零元素board[p][y]
+						board[x][y]*=2;		//合并到第一个非零元素
+						board[p][y]=0;		//另一个元素置为0
+						score+=board[x][y];//每合成一个元素，更新得分
+						x=p;				//从p开始继续往下寻找新的第一个非零元素，重复上述过程
+						need_add_random=1;  //每次有效移动后都需要添加新元素
 						break;
-					} else break;			//锟斤拷一锟斤拷元锟截诧拷锟斤拷同锟津不合诧拷
+					} else break;			//第一对元素不相同则不合并
 				}
 			}
 		}
-	for(y=0; y<SIZE; y++)    		//锟斤拷锟斤拷锟狡讹拷锟较诧拷锟斤拷锟斤拷元锟斤拷
+	for(y=0; y<SIZE; y++)    		//向上移动合并后的元素
 		for(x=0; x<SIZE; x++) {
 			if(board[x][y]==0);
 			else {
 				for(p=x; p>0&&board[p-1][y]==0; p--) {
 					board[p-1][y]=board[p][y];
-					board[p][y]=0;		//锟揭碉拷锟斤拷锟斤拷元锟截ｏ拷锟斤拷锟斤拷元锟斤拷锟斤拷锟斤拷锟叫空革拷锟斤拷锟斤拷锟斤拷锟狡碉拷没锟叫空革拷锟斤拷位锟斤拷
-					need_add_random=1;  //每锟斤拷锟斤拷效锟狡讹拷锟斤拷锟斤拷锟斤拷要锟斤拷锟斤拷锟斤拷元锟斤拷
+					board[p][y]=0;		//找到非零元素，若该元素上面有空格，则上移到没有空格的位置
+					need_add_random=1;  //每次有效移动后都需要添加新元素
 				}
 			}
 		}
 }
-void move_down() {				//锟斤拷转锟斤拷up锟斤拷锟斤拷锟斤拷锟斤拷锟狡讹拷
+void move_down() {				//旋转到up方向，再移动
 	rotate_board();
 	rotate_board();
-	move_up();					//锟狡讹拷锟斤拷锟斤拷锟斤拷转锟斤拷down锟斤拷锟斤拷
+	move_up();					//移动后，旋转回down方向
 	rotate_board();
 	rotate_board();
 }
 
-void move_left() {				//原锟斤拷同锟斤拷
+void move_left() {				//原理同上
 	rotate_board();
 	move_up();
 	rotate_board();
 	rotate_board();
 	rotate_board();
 }
-void move_right() {				//原锟斤拷同锟斤拷
+void move_right() {				//原理同上
 	rotate_board();
 	rotate_board();
 	rotate_board();
@@ -173,14 +174,14 @@ void move_right() {				//原锟斤拷同锟斤拷
 	rotate_board();
 }
 
-void add_random() {		//锟斤拷board锟斤拷锟斤拷锟斤拷锟斤拷一锟斤拷锟斤拷锟斤拷锟斤拷2锟斤拷锟斤拷4锟斤拷2锟侥革拷锟斤拷锟斤拷4锟斤拷锟斤拷锟斤拷锟斤拷
+void add_random() {		//向board数组添加一个随机数2或者4（2的概率是4的两倍）
 	int  n,count,x,y,temp,num;
 	if((count=count_empty())==0)return;
-	srand(time(0));			//锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷
-	temp=rand()%3;			//取锟斤拷锟斤拷锟斤拷0锟斤拷1锟斤拷2
-	if(temp==2)num=4;		//锟斤拷锟斤拷锟斤拷锟斤拷为2锟斤拷锟铰硷拷元锟斤拷为4锟斤拷锟斤拷锟斤拷锟铰硷拷元锟斤拷为2锟斤拷锟斤拷锟斤拷锟铰硷拷元锟斤拷锟斤拷2锟侥革拷锟斤拷锟斤拷4锟斤拷锟斤拷锟斤拷
+	srand(time(0));			//设置随机数种子
+	temp=rand()%3;			//取随机数0，1，2
+	if(temp==2)num=4;		//若随机数为2，新加元素为4，否则新加元素为2，所以新加元素是2的概率是4的两倍
 	else num=2;
-	n=(rand()%count)+1;//确锟斤拷锟铰硷拷元锟截碉拷位锟斤拷锟角碉拷n锟斤拷锟斤拷元锟斤拷
+	n=(rand()%count)+1;//确定新加元素的位置是第n个空元素
 	for(x=0; x<SIZE; x++)
 		for(y=0; y<SIZE; y++) {
 			if(board[x][y]==0&&--n==0) {
@@ -189,16 +190,16 @@ void add_random() {		//锟斤拷board锟斤拷锟斤拷锟斤拷锟斤拷一锟斤拷锟斤拷锟斤拷锟
 			}
 		}
 }
-int count_empty() {	//锟斤拷锟截碉拷前board锟斤拷锟斤拷锟斤拷为锟秸的革拷锟斤拷锟斤拷0锟斤拷示锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷
+int count_empty() {	//返回当前board数组中为空的个数（0表示棋盘已满）
 	int count=0,x,y;
 	for(x=0; x<SIZE; x++)
 		for(y=0; y<SIZE; y++)if(board[x][y]==0)count++;
 	return count;
 }
-void check_game_ended() {	//锟斤拷锟斤拷锟斤拷戏锟角凤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷1锟斤拷未锟斤拷锟斤拷锟斤拷锟斤拷0
+void check_game_ended() {	//检查游戏是否结束，结束返回1，未结束返回0
 	int x,y;
-	if(count_empty())return;//锟斤拷锟斤拷未锟斤拷锟斤拷锟斤拷锟介，锟截讹拷未锟斤拷锟斤拷
-	for(x=0; x<SIZE; x++) {	//锟斤拷锟斤拷锟斤拷锟斤拷锟饺斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟饺碉拷元锟截ｏ拷锟斤拷锟斤拷锟斤拷戏锟斤拷锟斤拷锟斤拷锟斤拷没锟斤拷锟斤拷锟斤拷戏锟斤拷锟斤拷
+	if(count_empty())return;//棋盘未满不检查，必定未结束
+	for(x=0; x<SIZE; x++) {	//横向纵向比较有无相邻相等的元素，有则游戏不结束，没有则游戏结束
 		for(y=0; y<SIZE-1; y++) {
 			if(board[x][y]==board[x][y+1]||board[y][x]==board[y+1][x]) {
 				game_ended=0;
@@ -208,9 +209,9 @@ void check_game_ended() {	//锟斤拷锟斤拷锟斤拷戏锟角凤拷锟斤拷锟斤拷锟斤拷锟斤拷
 	}
 	game_ended=1;
 }
-void check_game_win() {	//锟斤拷锟斤拷锟斤拷戏锟角凤拷胜锟斤拷锟斤拷胜锟斤拷game_win=1,未胜锟斤拷game_win=0
+void check_game_win() {	//检查游戏是否胜利，胜利game_win=1,未胜利game_win=0
 	int x,y;
-	for(x=0; x<SIZE; x++) {	//锟斤拷锟斤拷元锟斤拷2048锟斤拷锟斤拷胜锟斤拷
+	for(x=0; x<SIZE; x++) {	//若有元素2048，则胜利
 		for(y=0; y<SIZE; y++) {
 			if(board[x][y]==WINSCORE) {
 				game_win=1;
@@ -220,24 +221,24 @@ void check_game_win() {	//锟斤拷锟斤拷锟斤拷戏锟角凤拷胜锟斤拷锟斤拷胜锟斤拷game
 	}
 	game_win=0;
 }
-void print_board() {		//锟斤拷锟斤拷锟斤拷戏锟斤拷锟斤拷
-	system("cls");		//锟斤拷锟斤拷
+void print_board() {		//输出游戏棋盘
+	system("cls");		//清屏
 	printf("\n\n\n\n");
 	printf("                               GAME:    2048\n\n");
 	printf("                                 SCORE:%-6d\n\n",score);
-	printf("                        W:锟斤拷 A:锟斤拷 D:锟斤拷 S:锟斤拷 Q:quit\n");
+	printf("                        W:↑ A:← D:→ S:↓ Q:quit\n");
 	printf("             --------------------------------------------------\n\n");
-	printf("                        锟斤拷锟斤拷锟斤拷锟叫╋拷锟斤拷锟叫╋拷锟斤拷锟叫╋拷锟斤拷锟斤拷\n");
+	printf("                        ┌──┬──┬──┬──┐\n");
 	for (int i=0; i<SIZE; i++) {
-		printf("                        锟斤拷");
+		printf("                        │");
 		for (int j=0; j<SIZE; j++) {
-			if (board[i][j]!= 0)printf("%-4d锟斤拷", board[i][j]);
-			else printf("    锟斤拷");
+			if (board[i][j]!= 0)printf("%-4d│", board[i][j]);
+			else printf("    │");
 		}
 		if (i<SIZE-1) {
-			printf("\n                        锟斤拷锟斤拷锟斤拷锟洁─锟斤拷锟洁─锟斤拷锟洁─锟斤拷锟斤拷\n");
+			printf("\n                        ├──┼──┼──┼──┤\n");
 		} else {
-			printf("\n                        锟斤拷锟斤拷锟斤拷锟截╋拷锟斤拷锟截╋拷锟斤拷锟截╋拷锟斤拷锟斤拷\n");
+			printf("\n                        └──┴──┴──┴──┘\n");
 		}
 	}
 	printf("\n");
